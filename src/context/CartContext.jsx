7e58701d516036fb78b7b01364cb5513d81bc733
RahulRavMNNIT <rahulvermadbm@gmail.com>
2026-01-18 20:06:18 +0530

    shopping cart e-commerce

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,95 @@
+import { createContext, useContext, useState, useMemo } from "react";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+
+const CartContext = createContext();
+import { initialProducts } from "../data/product";
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+  const products = initialProducts;
+
+  //Adding item into the cart
+  const addTocart = (product) => {
+    toast.success("Item Added to Cart", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Bounce,
+    });
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === product.id);
+      if (existingItem) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: 1 }];
+      }
+    });
+  };
+
+  //Removing the item from the cart
+  const removeFromCart = (productId, removeAll = false) => {
+    toast.success("Item Remove From Cart", {
+      position: "top-right",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: false,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+      transition: Bounce,
+    });
+    setCart((prevCart) => {
+      const existingItem = prevCart.find((item) => item.id === productId);
+      if (!existingItem) return prevCart;
+      if (removeAll || existingItem.quantity === 1) {
+        return prevCart.filter((item) => item.id !== productId);
+      } else {
+        return prevCart.map((item) =>
+          item.id === productId
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        );
+      }
+    });
+  };
+
+  const clearCart = () => setCart([]);
+
+  const cartCount = useMemo(
+    () => cart.reduce((total, item) => total + item.quantity, 0),
+    [cart]
+  );
+
+  const cartTotal = useMemo(
+    () => cart.reduce((total, item) => total + item.price * item.quantity, 0),
+    [cart]
+  );
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        cart,
+        addTocart,
+        clearCart,
+        removeFromCart,
+        cartTotal,
+        cartCount,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = () => useContext(CartContext);
